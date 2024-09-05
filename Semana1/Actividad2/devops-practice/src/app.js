be20fb41bd8e3f205810e675520b325ab9a8a6b5
danicade24 <daniela.cadena.v@uni.pe>
2024-09-05 16:07:29 -0500

    Se edito los archivos src/app.js y tests/app.test.js

--- Semana1/Actividad2/devops-practice/src/app.js
+++ Semana1/Actividad2/devops-practice/src/app.js
@@ -1,15 +1,21 @@
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
- res.send('Hello, World!');
+    res.send('Hello, World!');
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
- console.log(`Server running on port ${port}`);
+app.get('/delay', (req, res) => {
+    setTimeout(() => {
+        res.send('This was delayed by 2 seconds');
+    }, 2000);
 });
 
 module.exports = app;
+
+if (require.main === module) {
+    const port = process.env.PORT || 3000; 
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
