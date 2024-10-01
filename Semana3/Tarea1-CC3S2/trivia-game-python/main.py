4fe10202d62717adfcc96118c876866b09621bb3
danicade24 <daniela.cadena.v@uni.pe>
2024-10-01 10:25:33 -0500

    Implement game logic in main.py

---
+++ Semana3/Tarea1-CC3S2/trivia-game-python/main.py
@@ -0,0 +1,15 @@
+from trivia import Question, Quiz
+
+def run_quiz():
+	quiz = Quiz()
+	while quiz.current_question_index < 10:
+		question = quiz.get_next_question()	#retorna la pregunta y si no hay retorna None
+		if question:	#Comprueba si quedan preguntas
+			print(question.description)
+			for idx, option in enumerate(question.options):	#el método devuelve tanto el ídice como el valor
+				print(f'{idx + 1}{option}')
+			answer = input("Tu respuesta: ")
+			if quiz.answer_question(question, answer):
+				print("¡Correcto!")
+			else:
+				break
