223a57c795b0c8ad03c2357d2cf1093b48e6316a
danicade24 <daniela.cadena.v@uni.pe>
2024-10-05 15:53:06 -0500

    Sprint 2: Add guess logic and hint system

---
+++ Semana4/Actividad9/word-guessing-game/src/game_logic.py
@@ -0,0 +1,58 @@
+import random
+
+def select_word():
+    """
+    Selección de una palabra al azar del archivo words.txt.
+    """
+    arr = []
+    try:
+        with open("words.txt") as file:
+            for line in file:
+                arr.append(line.strip())
+            return arr[random.randint(0, len(arr) - 1)]
+    except FileNotFoundError:
+        print("El archivo 'words.txt' no fue encontrado.")
+    
+        
+def guess_letter(word, letter, secret_word):
+    """
+    Recibe una letra del jugador y verifica si está en la palabra secreta.
+    Actualiza el progreso del jugador.
+    Controla los intentos restantes.
+    """
+    var = 0
+    for char in word:
+        if letter == char:
+            var += 1
+    
+    if var != 0:
+        print(f"¡Correcto! La letra '{letter}' está en la palabra")
+        replace(word, secret_word, letter)
+    else:
+        print(f"La letra '{letter}' no está en la palabra")
+        
+            
+def give_hint(word, secret_word):
+    """
+        Ofrece al jugador una pista revelando una letra aleatoria 
+        de la palabra secreta.
+        Limita el número de pistas disponibles.
+    """
+    hints = []
+    for char in word:
+        hints.append(char)
+    
+    hint = random.choice(hints)
+    #guess_letter(word, hint, secret_word)
+    print(f"\nLa letra '{hint}' está en la palabra.")
+    replace(word, secret_word, hint)
+    
+def replace(word,secret_word, letter):
+    for index, char in enumerate(word):
+            if char == letter:
+                secret_word[index] = letter
+            else:
+                pass
+    print(f"Palabra: ", end="")
+    print(" ".join(secret_word)) 
+    print()
--- Semana4/Actividad9/word-guessing-game/src/main.py
+++ Semana4/Actividad9/word-guessing-game/src/main.py
@@ -1,2 +1,26 @@
-print("Bienvenido al Juego de Adivinanza de Palabras!\n")
-print("La configuración inicial del juego se ha completado.")
+from game_logic import *
+
+def game():
+    print("Bienvenido al Juego de Adivinanza de Palabras!\n")
+    print("La configuración inicial del juego se ha completado.\n")
+    word = select_word()
+    if word:
+        print(f"La palabra ha sido seleccionada. ¡Comienza a adivinar!\n")
+    
+    print(" ".join(word) + '\n')    #imprime la palabra secreta
+    
+    secret_word = ["_"] * len(word)
+    
+    print(f"Palabra: ", end="")
+    print(" ".join(secret_word)) 
+    print()
+    
+    # Adivina una letra de la palabra
+    letter = input("Adivina una letra: ")
+    guess_letter(word, letter, secret_word)
+    
+    # Dar pista al jugador
+    give_hint(word, secret_word)
+    
+if __name__ == "__main__":
+    game()
